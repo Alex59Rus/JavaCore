c0de6ff3884782b9a71dcfa5a885ebeb4c35c937
Alex Oborin <cudlove.1@gmail.com>
2021-11-21 17:45:41 +0500

    M1.1 arrays

--- chapter03.java
+++ chapter03.java
@@ -60,3 +60,92 @@ class BoolTest {
         System.out.println("10>9 равно " + (10>9));//10>9 равно true
     }
 }
+class lifeTime {
+    public static void main(String[] args) {
+        int x;
+        for (int x = 0; x < 3; x++) {
+            int y = -1;
+            System.out.println("y равноЖ " + y);
+            y = 100;
+            System.out.println("Теперь у = " + y);
+
+        }
+    }
+}
+
+class Array {
+    public static void main(String[] args) {
+        int month_days[];
+        month_days = new int[12];
+        month_days[0] = 1;
+        month_days[1] =  2;
+        month_days[2] =  3;
+        month_days[3] =  30;
+        month_days[4] =  5;
+        month_days[5] =  6;
+        month_days[6] =  31;
+        month_days[7] =  28;
+        month_days[8] =  9;
+        month_days[9] =  0;
+        month_days[10] =  21;
+        month_days[11] =  123;
+        month_days[12] =  123;
+        System.out.println("В Апреле " + month_days[3] + " дней.");
+    }
+}
+class arrayUpdated{
+    public static void main(String[] args) {
+        int monthDays[] = {31, 17, 23, 43, 45, 67, 87, 30, 28};
+        System.out.println("В Апреле " + monthDays[8] + " дней.");
+    }
+}
+class average {
+    public static void main(String[] args) {
+        double nums[] = {10.1, 12.3, 12.4, 15.7, 12.8};
+        double result = 0;
+        int i;
+        for (int i = 0; i < 5; i++) {
+            result = result + nums[i];
+            System.out.println("Среднее значение равно " + result/5);
+        }
+    }
+}
+
+class twoArray {
+    public static void main(String[] args) {
+        int twoD[][] = new int[4][5];
+        int i, j, k = 0;
+        for (i = 0; i < 4; i++)
+            for (j = 0; j < 5; j++) {
+                twoD[i][j] = k;
+                k++;
+            }
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 5; j++)
+                System.out.print(twoD[i][j]+ " ");
+            System.out.println();
+        }
+    }
+}
+class twoArrayAgain{
+    public static void main(String[] args) {
+        int twoD[][] = new int[4][];
+        twoD[0] = new int[1];
+        twoD[1] = new int[2];
+        twoD[2] = new int[3];
+        twoD[3] = new int[4];
+        int i,j,k = 0;
+        for (i = 0; i < 4; i++)
+            for (j = 0; j < i+1; j++) {
+                twoD[i][j] = k;
+                k++;
+            }
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < i+1; j++)
+                System.out.print(twoD[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
+
+
