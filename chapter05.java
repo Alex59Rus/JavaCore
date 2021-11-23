46a1f7d56aea87ce58ef53311b4bb16dc2a04477
Alex Oborin <cudlove.1@gmail.com>
2021-11-23 16:07:35 +0500

    M1.1

---
+++ chapter05.java
@@ -0,0 +1,97 @@
+class I fElse {
+    puЫic static void main ( String args [ ] ) {
+        int month = 4; 11 Апрель
+        S t ring season;
+        i f (month == 12 11 month == 1 1 1 month == 2)
+        season = " зиме ";
+else i f ( month == З 11 month
+                season = " весне ";
+else i f ( month == 6 11 month
+                season = "лету";
+        4 11 month
+        7 1 1 month
+        5)
+        8)
+
+else i f (month == 9 11 month
+                season = " осени " ;
+        10 1 1 month == 11)
+else
+        season = " вЬIМЫШЛенным месяцам" ;
+        System . out . println ( "Aпpeль относится к"+ season + "." ) ;}}
+
+class SampleSwitch (
+        puЫic static void main ( St ring argз[] ) (
+for ( int i=O ; i<б; i++)
+        switch (i) (
+        case О:
+        Syзtem.out . println ( " i равно нулю . " ) ;
+        break;
+        case 1:
+        System . out . println ( " i равно единице . " );
+        break;
+        case 2:
+        Syзtem . out . println ( " i равно двум . " ) ;
+        break;
+        case 3:
+        System . out . println ( " i равно трем. " ) ;
+        break;
+        defaul t :
+        Syзtem . out . println ( " i больше трех . " ) ;}}}
+
+class Swi tch {
+    puЫic static void main ( St ring args[) ) {
+        int month = 4 ;
+        String season;
+        switch ( month )
+        case 1 2 :
+        case 1:
+        case 2:
+            season
+            break;
+        case 3:
+        case 4:
+        case 5 :
+            season
+            break;
+        case 6:
+        case 7:
+        case 8:
+            sea son
+            break;
+        case 9:
+        case 1 0 :
+        case 1 1 :
+            season
+            break;
+        de fault :
+        season
+        " эиме";
+
+        " весне ";
+
+        "лету";
+
+        "осени";
+        " вЬ1МЫ1ПЛенным мес яцам" ;
+
+        System.out . printl n ( "Aпpeль относится к" + season + "." ) ;}}
+
+class StringSwi tch {
+    puЫic static void main ( St ring args[] ) {
+        String str = "два";
+        switch ( str) {
+            case " один " :
+                System.out . println ( " oдин" );
+                break;
+            case "два " :
+                System.out . println ( "двa" );
+                break;
+            case " три" :
+                System.out . println ( " тpи" );
+
+                1 32 Часть 1. Язы к Java
+                break;
+            de faul t :
+            System . out . println ( " нe совпало ) " ) ;
+            brea k;}}
