1393078345d2646790be5aebb2d4f522026a260e
Alex Oborin <cudlove.1@gmail.com>
2021-11-26 03:37:57 +0500

    M1.

--- chapter05.java
+++ chapter05.java
@@ -1,76 +1,100 @@
 class IfElse {
-    public static void main (String args[]) {
+    public static void main(String args[]) {
         int month = 4; // Апрель
         String season;
         if (month == 12 || month == 1 || month == 2)
-                season = " зиме ";
-        else if ( month == 3 || month == 4 || month == 2)
-                season = "весне";
-        else if ( month == 6 || month == 7 || month == 8)
-                season = "лету";
-        else if (month == 9 || month == 10 || month == 11) {
-                season = "осени";
+            season = " зиме ";
+        else if (month == 3 || month == 4 || month == 2)
+            season = "весне";
+        else if (month == 6 || month == 7 || month == 8)
+            season = "лету";
+        else if (month == 9 || month == 10 || month == 11)
+            season = "осени";
         else
             season = "вымышленным месяцам";
-            System.out.println("Апрель относится к " + season +".");
+        System.out.println("Апрель относится к " + season + ".");
+    }
+}
+
+class SampleSwitch {
+    public static void main(String args[]) {
+        for (int i = O; i < б; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("i равно нулю .");
+                    break;
+                case 1:
+                    System.out.println(" i равно единице.");
+                    break;
+                case 2:
+                    System.out.println(" i равно двум.");
+                    break;
+                case 3:
+                    System.out.println(" i равно трем.");
+                    break;
+                default:
+                    System.out.println(" i больше трех.");
+            }
+        }
+    }
+}
+class MissingBreak {
+    public static void main(String[] args) {
+        for (int i = 0; i < 12; i++) {
+            switch (i) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    System.out.println("i меньше 5");
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    System.out.println("i <10");
+                    break;
+                default:
+                    System.out.println("i>=10");
+            }
+        }
+    }
+        }
 
 
-class SampleSwitch (
-        puЫic static void main ( St ring argз[] ) (
-for ( int i=O ; i<б; i++)
-        switch (i) (
-        case О:
-        Syзtem.out . println ( " i равно нулю . " ) ;
-        break;
-        case 1:
-        System . out . println ( " i равно единице . " );
-        break;
-        case 2:
-        Syзtem . out . println ( " i равно двум . " ) ;
-        break;
-        case 3:
-        System . out . println ( " i равно трем. " ) ;
-        break;
-        defaul t :
-        Syзtem . out . println ( " i больше трех . " ) ;}}}
-
-class Swi tch {
-    puЫic static void main ( St ring args[) ) {
+class Switch {
+    public static void main (String args[]) {
         int month = 4 ;
         String season;
-        switch ( month )
-        case 1 2 :
-        case 1:
-        case 2:
-            season
-            break;
-        case 3:
-        case 4:
-        case 5 :
-            season
-            break;
-        case 6:
-        case 7:
-        case 8:
-            sea son
-            break;
-        case 9:
-        case 1 0 :
-        case 1 1 :
-            season
-            break;
-        de fault :
-        season
-        " эиме";
-
-        " весне ";
-
-        "лету";
-
-        "осени";
-        " вЬ1МЫ1ПЛенным мес яцам" ;
-
-        System.out . printl n ( "Aпpeль относится к" + season + "." ) ;}}
+        switch (month) {
+            case 12:
+            case 1:
+            case 2:
+                season = "зиме";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "весне";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "лету";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "осени";
+                break;
+            default:
+                season = "вымышленным месяцам";
+        }
+        System.out.println("Aпpeль относится к " + season + "." ) ;
+    }
+}
 
 class StringSwi tch {
     puЫic static void main ( St ring args[] ) {
