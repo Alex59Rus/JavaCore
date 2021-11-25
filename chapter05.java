9976801b2d5d12c317e4ac84625c103707998f97
Alex Oborin <cudlove.1@gmail.com>
2021-11-26 04:01:52 +0500

    M1. menu

--- chapter05.java
+++ chapter05.java
@@ -116,3 +116,53 @@ class StringSwitch {
     }
 }
 
+class Menu {
+    public static void main(String[] args) {
+        throws java.io.IOEXception{
+            char choice;
+        do {
+            System.out.println("Справка по оператору");
+            System.out.println("    1. if");
+            System.out.println("    2. switch");
+            System.out.println("    3. while");
+            System.out.println("    4. do-while");
+            System.out.println("    5. for\n");
+            System.out.println("Выберите нужный пункт");
+            choice = (char) System.in.read();
+        } while (choice < '1' || choice > '5');
+        System.out.println("\n");
+
+        switch (choice){
+            case '1':
+                System.out.println("if:\n") ;
+                System.out.println("if(ycлoвиe) оператор;") ;
+                System.out.println("else оператор;" ) ;
+                break;
+            case '2' :
+            System.out.println("switch:\n");
+            System.out.println("switch(выpaжeниe){") ;
+            System.out.println("саsе константа : ");
+            System.out.println("последовательность операторов");
+            System.out.println("break;");
+            System.out.println(" // ... ");
+            System.out.println("}");
+            break;
+            case 'З':
+            System.out.println( "while : \n" ) ;
+            System.out.println( "while (ycлoвиe) оператор ;");
+            break;
+            case '4':
+                System.out.println( " do-whi le : \n " );
+                System.out.println( "do {");
+                System.out.println(" оператор ; " );
+                System.out.println("} while ( условие ) ;");
+                break;
+            case '5':
+                System.out.println("for : \n");
+                System.out.print("for ( инициaлиэaция; условие ; итерация } ");
+                System.out.println(" оператор ; ") ;
+                break;
+        }
+        }
+    }
+}
