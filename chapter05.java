723f12391fe8ce55db7cb19355a7a08cca7c2509
Alex Oborin <cudlove.1@gmail.com>
2021-11-26 03:43:35 +0500

    M1.

--- chapter05.java
+++ chapter05.java
@@ -96,21 +96,23 @@ class Switch {
     }
 }
 
-class StringSwi tch {
-    puЫic static void main ( St ring args[] ) {
+class StringSwitch {
+    public static void main(String args[]) {
         String str = "два";
-        switch ( str) {
-            case " один " :
-                System.out . println ( " oдин" );
+        switch (str) {
+            case "один":
+                System.out.println("oдин");
                 break;
-            case "два " :
-                System.out . println ( "двa" );
+            case "два":
+                System.out.println("двa");
                 break;
-            case " три" :
-                System.out . println ( " тpи" );
+            case "три":
+                System.out.println("тpи");
+                break;
+            default:
+                System.out.println("нe совпало");
+                break;
+        }
+    }
+}
 
-                1 32 Часть 1. Язы к Java
-                break;
-            de faul t :
-            System . out . println ( " нe совпало ) " ) ;
-            brea k;}}
