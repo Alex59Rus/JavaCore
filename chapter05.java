3c6ce5b688e03c0c50adecd67c9d93e005bf616d
Alex Oborin <cudlove.1@gmail.com>
2021-11-26 09:50:19 +0500

    M1. end

--- chapter05.java
+++ chapter05.java
@@ -166,3 +166,111 @@ class Menu {
         }
     }
 }
+
+class FindPrime {
+    public static void main (String args[]) {
+        int num;
+        boolean isPrime ;
+        num = 14;
+        if (num < 2) isPrime = false;
+            else isPrime true ;
+        for (int i=2 ; i <= num/i ; i++) {
+            if ((num % i) == 0) {
+                isPrime = false;
+                break;
+                if (isPrime) System.out.println("Пpocтoe число");
+                else System.out.println("He простое число");
+            }
+        }
+    }
+}
+class Comma {
+    public static void main(String args[]) {
+    int а, b;
+for(a=1,b=4;а<b;а++,b--) {
+        System.out.println("a = " + а);
+        System.out.println("b = " + b);
+        }
+    }
+}
+class forEach {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4,5,6,7,8,9};
+        int sum =0;
+        for(int x : nums) {
+            System.out.println("Значение равно: " + x);
+            sum += x;
+        }
+        System.out.println("Сумма равна: " + sum);
+    }
+}
+class ForEach2 {
+    public static void maiп (String args[]) {
+        int sum = О;
+        int nums [] = { 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10};
+        for ( int x : nums ) {
+            System.out.println("Знaчeниe равно : " + x);
+            sum += х;
+            if (x == 5) break;
+        }
+            System.out.println("Cyммa пяти первых элементов равна : " + sum);
+        }
+    }
+class forEach3 {
+    public static void main(String[] args) {
+        int sum =0;
+        int nums[][] = new int[3][5];
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < ; j++)
+                nums[i][j] = (i+1)*(j+1);
+            for(int x[] : nums) {
+                for(int y : x) {
+                    System.out.println("значение равно: " + y);
+                    sum += y;
+                }
+            }
+        System.out.println("Сумма: " + sum);
+    }
+}
+class nested {
+    public static void main(String[] args) {
+        int i,j;
+        for (i = 0; i < 10; i++) {
+            for (j = 0; j < 10; j++)
+                System.out.print(".");
+            System.out.println();
+        }
+    }
+
+}
+class BreakLoop {
+    public static void maiп(String args[]) {
+        for (int i = O; i < 100; i++) {
+            if (i == 10) break;
+            System.out.println("i: " + i);
+        }
+            System.out.println("Цикл завершен.");
+    }
+}
+class ContinueLabel {
+    public static void main (String args[]) {
+    outer : for ( int i=0 ; i< 10; i++ )
+        for (int j = 0; j < 10; j++) {
+            if (j > i) {
+                System.out.println();
+                continue outer;
+                System.out.print(" " + (i * j));
+                }
+            }
+            Systern.out.println();
+        }
+    }
+
+class Return {
+    public static void main(String args[]) {
+        boolean t = true;
+        System.out.println("Дo возврата. ");
+        if (t) return;
+        Systern.out.println(" Этoт оператор выполняться не будет. ");
+    }
+}
